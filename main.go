716f7f13dfea4473fd2ba50c072abe7454ad8b5f
sunnivalien <89248508+sunnivalien@users.noreply.github.com>
2022-01-28 13:43:27 +0100

    commit on main

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import github.com/sunnivalien/myqoute/myqoute
+
+func main() {
+	myquote.Glass()
+	myquote.Go()
+	myquote.Hello()
+	myquote.Opt()
+}
---
+++ myquote/myquote.go
@@ -0,0 +1,23 @@
+package myquote
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func Glass() {
+	fmt.Println(quote.Glass())
+}
+
+func Go() {
+	fmt.Println(quote.Go())
+}
+
+func Hello() {
+	fmt.Println(quote.Hello())
+}
+
+func Opt() {
+	fmt.Println(quote.Opt())
+}
